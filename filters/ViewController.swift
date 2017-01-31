0d129f9363b6809d0b6520881c7d467baf6ab1fb
Toni Jovanoski <minimoog77@gmail.com>
2017-01-31 02:22:24 +0100

    Some fixes

--- filters/ViewController.swift
+++ filters/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CameraCaptureHelperDelegate {
 
+    let imageView = MetalImageView()
+    
+    let cameraCaptureHelper = CameraCaptureHelper(cameraPosition: .back)
+    
+    let crystalize = CIFilter(name: "CICrystallize", withInputParameters: [kCIInputRadiusKey: 30])!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(imageView)
+        
+        cameraCaptureHelper.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidLayoutSubviews() {
+        imageView.frame = view.bounds.insetBy(dx: 50, dy: 50)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func newCameraImage(_ cameraCaptureHelper: CameraCaptureHelper, image: CIImage) {
+        crystalize.setValue(image, forKey: kCIInputImageKey)
+        
+        imageView.image = crystalize.outputImage
+    }
 }
 
